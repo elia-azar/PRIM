a7a258c2b705cc1b0f87228ede302b51f5d0e34c
Elia Azar <elia.azar@polytechnique.edu>
2021-04-05 22:23:10 +0200

    bug fix in scripts

--- filter_loss/dump_automate.sh
+++ filter_loss/dump_automate.sh
@@ -2,22 +2,26 @@
 
 num=0
 filter=""
-echo 'STARTING Moongen traffic generator'
+filt_num=0
 
-while getopts ":n:f:" arg; do
+while getopts ":n:f:m:" arg; do
 	case "${arg}" in
 		n)
 			num=${OPTARG}
 			;;
-                f)
-                        filter=${OPTARG}
-                        ;;
+		f)
+			filter=${OPTARG}
+			;;
+		m)
+			filt_num=${OPTARG}
+			;;
 	esac
 done
 
+echo $filter
 
-timeout 70s ./moongen_dump_pcap.sh -f $filter > test.txt
+timeout 70s ./moongen_dump_pcap.sh -f "${filter}" > test.txt
 
 sleep 2
-echo "New results" >> results_dump${num}_${filter}.txt
-cat test.txt | awk '/StdDev/ {print $0} {}' >> results_dump${num}_${filter}.txt
+echo "New results" >> results_dump${num}_${m}.txt
+cat test.txt | awk '/StdDev/ {print $0} {}' >> results_dump${num}_${m}.txt
