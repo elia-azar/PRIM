bd7ad97e2afe557f6106ee065157ad10b366fb7f
Elia Azar <elia.azar@polytechnique.edu>
2021-04-17 23:06:51 +0200

    debug packet recieved by interface running bcc

--- bcc/pkt-filter.py
+++ bcc/pkt-filter.py
@@ -70,10 +70,9 @@ socket_fd = function_udp_filter.sock
 #create python socket object, from the file descriptor
 sock = socket.fromfd(socket_fd,socket.AF_NETLINK,socket.SOCK_DGRAM)
 #set it as blocking socket
-#sock.setblocking(True)
+sock.setblocking(True)
 
 while 1:
-    """
     #retrieve raw packet from socket
     packet_str = os.read(socket_fd,256)
 
@@ -84,18 +83,71 @@ while 1:
     #convert packet into bytearray
     packet_bytearray = bytearray(packet_str)
 
+    print("-------------------------------------")
+    i = 0
+    for val in packet_bytearray:
+        str_val = str(hex(val))[2:]
+        str_val = str_val if len(str_val) != 1 else "0" + str_val
+        print(str_val + " ", end = "")
+        i += 1
+        if i % 8 == 0:
+            print("  ", end = "")
+        if i % 16 == 0:
+            print()
+    
+    print()
+    print()
+
+    """
     #ethernet header length
     ETH_HLEN = 14
+
+    src_mac = ""
+    dst_mac = ""
+    for i in range(6): 
+        src_mac += str(hex(packet_bytearray[i]))[2:] + ":"
+        dst_mac += str(hex(packet_bytearray[6+i]))[2:] + ":"
+    
+    src_mac = src_mac[:-1]
+    dst_mac = src_mac[:-1]
+
+    ether_type = str((packet_bytearray[ETH_HLEN - 2] << 8) + packet_bytearray[ETH_HLEN - 1])
+
+    #calculate packet total length
+    total_length = packet_bytearray[ETH_HLEN + 2]               #load MSB
+    total_length = total_length << 8                            #shift MSB
+    total_length = total_length + packet_bytearray[ETH_HLEN+3]  #add LSB
+
+    src_addr = ""
+    dst_addr = ""
+    for i in range(4):
+        src_addr += str(packet_bytearray[ETH_HLEN + 12 + i]) + "."
+        dst_addr += str(packet_bytearray[ETH_HLEN + 16 + i]) + "."
+    
+    src_addr = src_addr[:-1]
+    dst_addr = src_addr[:-1]
 
     #calculate ip header length
     ip_header_length = packet_bytearray[ETH_HLEN]               #load Byte
     ip_header_length = ip_header_length & 0x0F                  #mask bits 0..3
     ip_header_length = ip_header_length << 2                    #shift to obtain length
 
-    #retrieve udp destination port
-    dst_port = packet_bytearray[ETH_HLEN + ip_header_length + 2]
-    dst_port = dst_port << 8
-    dst_port = dst_port + packet_bytearray[ETH_HLEN+ ip_header_length +3]
+    #retrieve transport protocol
+    proto = packet_bytearray[ETH_HLEN + 9]
+
+    #retrieve source port
+    src_port = packet_bytearray[ETH_HLEN + ip_header_length]           #load Byte
+    src_port = src_port << 8                                               #shift MSB
+    src_port = src_port + packet_bytearray[ETH_HLEN+ ip_header_length + 1]  #add LSB
+    
+    #retrieve destination port
+    dst_port = packet_bytearray[ETH_HLEN + ip_header_length + 2]           #load Byte
+    dst_port = dst_port << 8                                               #shift MSB
+    dst_port = dst_port + packet_bytearray[ETH_HLEN+ ip_header_length +3]  #add LSB
+
+    print("--------------")
+    print(src_mac + ", " + dst_mac + " with ether_type:" + ether_type+ " and proto:" + str(proto) + ", " + src_addr + ":" + str(src_port) + " -> " + dst_addr + ":" + str(dst_port))
+    """
     """
     sleep(2)
     #print stats
@@ -106,3 +158,4 @@ while 1:
         print(s)
     else:
         print("No entries yet")
+    """
