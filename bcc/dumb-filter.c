83cdf2ff92c7798ef117fdda3a02f50ee10aab1f
Elia Azar <elia.azar@polytechnique.edu>
2021-04-19 20:14:06 +0200

    dumb-filter implementation

---
+++ bcc/dumb-filter.c
@@ -0,0 +1,23 @@
+#include <uapi/linux/ptrace.h>
+#include <net/sock.h>
+#include <bcc/proto.h>
+
+
+BPF_HASH(pkt_count);
+
+/*eBPF program.
+  Accepts all the incoming packets and increments the counter for every packet
+*/
+int dumb_filter(struct __sk_buff *skb) {
+
+    u64 key = 1;
+    u64 counter = 0;
+
+    p = pkt_count.lookup(&key);
+    if (p != 0) {
+        counter = *p;
+    }
+    counter++;
+    pkt_count.update(&key, &counter);
+    return -key;
+}
