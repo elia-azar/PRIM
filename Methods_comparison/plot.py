606fe304f486b5cf6126a942e35877de04f10ff7
Elia Azar <elia.azar@polytechnique.edu>
2021-04-19 22:15:32 +0200

    results for bcc

--- Methods_comparison/plot.py
+++ Methods_comparison/plot.py
@@ -6,20 +6,23 @@ tcpdump = [3854197, 97485587, 98667143, 12322299]
 moongen = [279853, 212805119, 518657863, 448772404]
 pdump = [0, 102185086, 158297349, 0]
 xdp = [892457, 97197078, 103568567, 66722471]
+bcc = [1412441, 4190632, 61040784, 59974560]
 
 tcpdump_loss = [99.56, 88.71, 88.53, 98.57]
 moongen_loss = [99.97, 75.33, 40.01, 48.13]
 pdump_loss = [0, 88.20,  81.75, 0]
 xdp_loss = [99.9, 88.67, 88, 92.24]
+bcc_loss = [99.84, 99.52, 92.69, 93.12]
 
 x = np.arange(len(labels))  # the label locations
 width = 0.35  # the width of the bars
 
 fig, ax = plt.subplots()
-rects1 = ax.bar(x - width/2 + width/8, tcpdump_loss, width/4, label='tcpdump')
-rects2 = ax.bar(x - width/8, moongen_loss, width/4, label='moongen')
-rects3 = ax.bar(x + width/8, xdp_loss, width/4, label='xdp-dump')
-rects4 = ax.bar(x + width/2 - width/8, pdump_loss, width/4, label='pdump')
+rects1 = ax.bar(x - 2*width/5, tcpdump_loss, width/5, label='tcpdump')
+rects2 = ax.bar(x - width/5, moongen_loss, width/5, label='moongen')
+rects3 = ax.bar(x, xdp_loss, width/5, label='xdp-dump')
+rects4 = ax.bar(x + width/5, bcc_loss, width/5, label='bcc')
+rects5 = ax.bar(x + 2*width/5, pdump_loss, width/5, label='pdump')
 
 # Add some text for labels, title and custom x-axis tick labels, etc.
 ax.set_ylabel('Packet Loss')
@@ -33,10 +36,11 @@ fig.tight_layout()
 plt.savefig("pkt_loss.png")
 
 fig, ax = plt.subplots()
-rects1 = ax.bar(x - width/2 + width/8, tcpdump, width/4, label='tcpdump')
-rects2 = ax.bar(x - width/8, moongen, width/4, label='moongen')
-rects3 = ax.bar(x + width/8, xdp, width/4, label='xdp-dump')
-rects4 = ax.bar(x + width/2 - width/8, pdump, width/4, label='pdump')
+rects1 = ax.bar(x - 2*width/5, tcpdump, width/5, label='tcpdump')
+rects2 = ax.bar(x - width/5, moongen, width/5, label='moongen')
+rects3 = ax.bar(x, xdp, width/5, label='xdp-dump')
+rects4 = ax.bar(x + width/5, bcc, width/5, label='bcc')
+rects5 = ax.bar(x + 2*width/5, pdump, width/5, label='pdump')
 
 # Add some text for labels, title and custom x-axis tick labels, etc.
 ax.set_ylabel('RX packets')
