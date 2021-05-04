0569c8c1a43096edc71f859829fa249dfc543c59
Elia Azar <elia.azar@polytechnique.edu>
2021-05-04 11:58:04 +0200

    bug fix for dpdk-pdump benchmark script

--- src/dpdkpdump_automate.sh
+++ src/dpdkpdump_automate.sh
@@ -3,9 +3,10 @@
 num=0
 
 # make sure that the following is up and running in a separate shell
-# dpdk-devbind -b 04:00.0 uio_pci_generic
+# dpdk-devbind -b uio_pci_generic 04:00.0
+# cd /opt/tools/dpdk-20.11/build/app
 # ./dpdk-testpmd -l 0,1-5 -n 4 -a 0000:04:00.0 -- -i --port-topology=chained
-# testpmd >set fwd io
+# testpmd > set fwd io
 # testpmd > start
 
 echo 'STARTING DPDK-PDUMP'
@@ -18,9 +19,10 @@ while getopts ":n:" arg; do
 	esac
 done
 
+cd /opt/tools/dpdk-20.11/build/app
+timeout 70s -s SIGINT ./dpdk-pdump -l 6,7-10 -n 4 -- --pdump 'port=0,queue=*,rx-dev=/dev/null' &> $CONFIG_DIR/test.txt
 
-timeout 70s ./dpdk-pdump -l 6,7-10 -n 4 -- --pdump 'port=0,queue=*,rx-dev=/dev/null' &> test.txt
+cd $CONFIG_DIR
 
-sleep 2
 echo "New results" >> results_dpdkpdump${num}.txt
 cat test.txt | awk '/packets/ {print $0} {}' >> results_dpdkpdump${num}.txt
